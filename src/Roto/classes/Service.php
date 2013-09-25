d2047aaf8f8dcc971f340c899b8f9a37c7fb0924
Ray Minge <the@rayminge.com>
2013-09-25 17:33:28 -0500

    Moving classes into Roto folder

---
+++ src/Roto/classes/Service.php
@@ -0,0 +1,38 @@
+<?php
+
+class Service {
+	
+	private static $lambdaRegistry = array();
+	private static  $cache = array();
+
+	private static function isCached($key) {
+		return isset(self::$cache[$key]);
+	}
+
+	public static function isRegistered($key) {
+		return isset(self::$lambdaRegistry[$key]);
+	}
+
+	public static function __callStatic($name, $args) {
+		return self::get($name);
+	}
+
+	public static function get($key) {
+		if (! self::isRegistered($key)) {
+			throw new Exception("Service `$key' not registered");
+		}
+		if (! self::isCached($key)) {
+			$lambda = self::$lambdaRegistry[$key];
+			self::$cache[$key] = $lambda();
+		}
+		return self::$cache[$key];
+
+	}
+
+	public static function register($key, $lambda, $override = false) {
+		if (self::isRegistered($key) && ! $override) {
+			throw new Exception("Service `$key' is already registered");
+		}
+		self::$lambdaRegistry[$key] = $lambda;
+	} 
+}
