6c29bdd48fd222cdd3fd64c4eb990e6a4a234f0c
Raymond Minge <ray@rayminge.com>
2013-12-09 16:42:29 -0600

    Config files can now use  keys

--- src/Roto/Config.php
+++ src/Roto/Config.php
@@ -6,12 +6,13 @@ class Config {
 
 	private $data = null;
 	private $iniFiles = array();
+	private $serverReplace = array();
 
 	private function loadConfig() {
 		if (is_null($this->data)) {
 			$numInis = count($this->iniFiles);
-			$this->data = parse_ini_file($this->iniFiles[$numInis - 1], true);
-			for ($i = $numInis - 2; $i >= 0; $i--) {
+			$this->data = array();//parse_ini_file($this->iniFiles[$numInis - 1], true);
+			for ($i = $numInis - 1; $i >= 0; $i--) {
 				$overrideData = parse_ini_file($this->iniFiles[$i], true);
 				foreach ($overrideData as $section => $values) {
 					if (is_array($values)) {
@@ -19,24 +20,53 @@ class Config {
 							$this->data[$section] = array();
 						}
 						foreach ($values as $key => $value) {
-							$this->data[$section][$key] = $value;
+							$this->data[$section][$key] = $this->substitute($value, $_SERVER);
 						}
 					} else {
-						$this->data[$section] = $value;
+						$this->data[$section] = $this->substitute($value, $_SERVER);
 					}
 				}
 			}
 		}
 	}
 
-	public function __construct() {	
-		$this->iniFiles = func_get_args();
+	private function substitute($string, $matches = null, $default_template = '{{%s}}') {
+		while (preg_match("/\{\[(.*?)\]\}/", $string, $var_matches)) {
+			if (is_null($matches)) {
+				throw new \Exception("Cannot use at substitution without configuring a map");
+			}
+
+			$search = $var_matches[0];
+			$name = $var_matches[1];
+
+			if (isset($matches[$name])) {
+				$replace = $matches[$name];
+			} else {
+				$replace = sprintf($default_template, $name);
+			}
+
+			$string = str_replace($search, $replace, $string);
+
+		}
+		return $string;
+	}
+
+	public function __construct() {
+		$args = func_get_args();
+		$last_arg = $args[count($args) - 1];
+		if (is_array($last_arg)) {
+			$this->iniFiles = array_slice($args, 0, -1);
+			$this->serverReplace = $last_arg;
+		} else {
+			$this->iniFiles = $args;
+			$this->serverReplace = null;
+		}
 	}
 
 	public function __get($name) {
 		$this->loadConfig();
 		if (! array_key_exists($name, $this->data)) {
-			throw new Exception("Invalid database key `$name'");
+			throw new \Exception("Invalid database key `$name'");
 		}
 		return $this->data[$name];
 	}
